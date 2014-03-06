e5307a647d02665d7144addc237f4645be383990
tonyeung <tonyeung85@gmail.com>
2014-03-06 09:31:55 -0600

    Update welcome.js

--- source/plugins/welcome.js
+++ source/plugins/welcome.js
@@ -1,12 +1,10 @@
 (function () {
 "use strict";
 
-var message = "Welcome to the JavaScript chat! Please review the " +
-		bot.adapter.link(
-			"room pseudo-rules",
-			"http://rlemon.github.com/so-chat-javascript-rules/" ) + ". " +
-	"Please don't ask if you can ask or if anyone's around; just ask " +
-	"your question, and if anyone's free and interested they'll help.";
+var message = "Welcome to the Maid Café! The only rule is to be nice." +
+	"Welcome back, my Master! お帰りなさいませ、ご主人様! " + 
+	"We're now serving the Animu & Mangos with a side of Moé. " + 
+	"Sit back and relax with general chat for anime.stackexchange.com";
 
 function welcome ( name ) {
 	return bot.adapter.reply( name ) + " " + message; ;
