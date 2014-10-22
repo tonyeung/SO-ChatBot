0cccd264fe4f8a749eeff8c6814cb2bc8e43d610
tonyeung <tonyeung85@gmail.com>
2014-10-21 19:39:48 -0500

    fixed SO change

--- master.js
+++ master.js
@@ -2377,7 +2377,7 @@ bot.adapter = {
 		this.fkey = fkey.value;
 		this.roomid = Number( /\d+/.exec(location)[0] );
 		this.site = /chat\.(\w+)/.exec( location )[ 1 ];
-		this.user_id = CHAT.user.current().id;
+		this.user_id = CHAT.RoomUsers.current().id;
 
 		this.in.init();
 		this.out.init();
