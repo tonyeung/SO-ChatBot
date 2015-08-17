8c40fa244edfdf713f235eb8b33d7a2c1d8f5e15
tonyeung <tonyeung85@gmail.com>
2015-08-16 23:58:36 -0500

    fixed reference to raw.github.com (MIME error)

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var a=document.createElement("script");a.src="https://raw.github.com/tonyeung/SO-ChatBot/master/master.js";document.head.appendChild(a);})()
+javascript:(function(){var a=document.createElement("script");a.src="https://rawgit.com/tonyeung/SO-ChatBot/master/master.js";document.head.appendChild(a);})()
